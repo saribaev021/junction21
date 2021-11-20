c6db103315ce2b415027b67eedc9e4ac64408cb2
zcolleen <semeneigin@yandex.ru>
2021-11-20 21:55:59 +0300

    log made

--- app-api/internal/server/handlers.go
+++ app-api/internal/server/handlers.go
@@ -9,6 +9,11 @@ import (
 	"time"
 )
 
+var (
+	serializeError = "serialize error: %s"
+	dbError        = "database error: %s"
+)
+
 func (s *Server) createTaskHandler(writer http.ResponseWriter, request *http.Request) {
 	data := struct {
 		Name        string    `json:"Name"`
@@ -19,7 +24,8 @@ func (s *Server) createTaskHandler(writer http.ResponseWriter, request *http.Req
 	}{}
 
 	if err := json.NewDecoder(request.Body).Decode(&data); err != nil {
-		http.Error(writer, fmt.Sprintf("serialize error: %s", err), http.StatusBadRequest)
+		http.Error(writer, fmt.Sprintf(serializeError, err), http.StatusBadRequest)
+		log.Printf(serializeError, err)
 		return
 	}
 	log.Printf("incoming task: %v", data)
@@ -30,6 +36,7 @@ func (s *Server) createTaskHandler(writer http.ResponseWriter, request *http.Req
 	newTask.UserId, err = s.dbHandler.GetUserIdByName(data.UserName)
 	if err != nil {
 		http.Error(writer, fmt.Sprintf("database error: %s", err.Error()), http.StatusInternalServerError)
+		log.Printf(dbError, err)
 		return
 	}
 
@@ -39,6 +46,7 @@ func (s *Server) createTaskHandler(writer http.ResponseWriter, request *http.Req
 	newTask.StartDate = data.StartDate
 	if err := s.dbHandler.CreateTask(*newTask); err != nil {
 		http.Error(writer, fmt.Sprintf("database error: %s", err.Error()), http.StatusInternalServerError)
+		log.Printf(dbError, err)
 		return
 	}
 
@@ -52,24 +60,27 @@ func (s *Server) getTasksHandler(writer http.ResponseWriter, request *http.Reque
 	log.Printf("query name: %s", name)
 	if name == "" {
 		http.Error(writer, fmt.Sprintf("empty name"), http.StatusBadRequest)
+		log.Println("empty name")
 		return
 	}
 
 	userId, err := s.dbHandler.GetUserIdByName(name)
 	if err != nil {
 		http.Error(writer, fmt.Sprintf("db error: %s", err.Error()), http.StatusBadRequest)
+		log.Printf(dbError, err)
 		return
 	}
 
 	tasks, err := s.dbHandler.GetUserTasks(userId)
 	if err != nil {
 		http.Error(writer, fmt.Sprintf("db error: %s", err.Error()), http.StatusBadRequest)
+		log.Printf(dbError, err)
 		return
 	}
 
-	log.Println(tasks)
 	if err := json.NewEncoder(writer).Encode(tasks); err != nil {
 		http.Error(writer, fmt.Sprintf("serialize error: %s", err.Error()), http.StatusInternalServerError)
+		log.Printf(serializeError, err)
 		return
 	}
 
