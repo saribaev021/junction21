e77eddf23c6eae74fa3424b23aa227e216434423
zcolleen <semeneigin@yandex.ru>
2021-11-20 20:18:23 +0300

    java added

---
+++ junction/src/main/java/com/app/UserInfo/entity/UsersEntity.java
@@ -0,0 +1,67 @@
+package com.app.UserInfo.entity;
+
+import javax.persistence.*;
+import java.util.Arrays;
+
+@Entity
+@Table(name = "users")
+public class UsersEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private int rating;
+    private byte[] photo;
+
+    public UsersEntity() {
+    }
+
+    public UsersEntity(Integer id, String name, int rating, byte[] photo) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+        this.photo = photo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+    @Override
+    public String toString() {
+        return "UserEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", rating=" + rating +
+                ", photo=" + Arrays.toString(photo) +
+                '}';
+    }
+}
